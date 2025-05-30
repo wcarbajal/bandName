52c831299485651d20acc7822b568e6eb4242fca
wuilmer carbajal <wcarbajal@outlook.com>
2025-05-30 00:13:57 -0500

    Inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BandAdd } from './components/BandAdd';
+import { BandList } from './components/BandList';
+
+function App() {
+
+  return (
+    <div className="container">
+
+      <div className="alert">
+        <p>
+          Services status:
+          <span className=" text-success" > Online</span>
+          <span className=" text-danger" > Offline</span>
+
+        </p>
+      </div>
+
+
+      <h1>BandNames</h1>
+
+      <hr />
+
+      <div className="row">
+        <div className="col-8">
+          <BandList />
+        </div>
+
+        <div className="col-4">
+          <BandAdd />
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
